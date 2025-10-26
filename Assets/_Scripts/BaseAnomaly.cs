78bc25cad062aed5f2d21771e080924a1d86e8dc
thanhbinh1907 <binh50135@gmail.com>
2025-10-26 17:32:58 +0700

    s

---
+++ Assets/_Scripts/BaseAnomaly.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+// Đây là lớp "cha"
+// Bất kỳ script anomaly nào cũng sẽ kế thừa từ lớp này
+public abstract class BaseAnomaly : MonoBehaviourPun
+{
+	// Tất cả anomaly BẮT BUỘC phải có hàm Kích hoạt
+	// Đây là nơi logic anomaly xảy ra (hiện bao xác, tắt đèn, v.v.)
+	public abstract void ActivateAnomaly();
+
+	// Và một hàm Vô hiệu hóa để reset station
+	public abstract void DeactivateAnomaly();
+}
---
+++ Assets/_Scripts/StationEntryTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StationEntryTrigger : MonoBehaviour
+{
+	// Kéo StationAnomalyManager của trạm này vào
+	public StationAnomalyManager anomalyManager;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// Giả sử chỉ người chơi (Player) mới kích hoạt
+		if (other.CompareTag("Player"))
+		{
+			// Kích hoạt logic anomaly!
+			anomalyManager.InitializeStation();
+		}
+	}
+}
